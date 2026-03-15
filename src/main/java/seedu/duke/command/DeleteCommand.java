46dc928ee52b271ea4df571fce8e8c9e60b83fd0
Ariff Muhammed <ariffahsan@gmail.com>
2026-03-15 22:58:54 +0800

    feat: add assertions and logging to DeleteCommand and SpendTrack

--- src/main/java/seedu/duke/command/DeleteCommand.java
+++ src/main/java/seedu/duke/command/DeleteCommand.java
@@ -1,5 +1,7 @@
 package seedu.duke.command;
 
+import java.util.logging.Logger;
+
 import seedu.duke.Expense;
 import seedu.duke.ExpenseList;
 import seedu.duke.SpendTrackException;
@@ -9,6 +11,12 @@ import seedu.duke.Ui;
  * Deletes an expense from the expense list by its 1-based index.
  */
 public class DeleteCommand extends Command {
+
+    private static final Logger logger = Logger.getLogger(DeleteCommand.class.getName());
+
+    static {
+        logger.setUseParentHandlers(false);
+    }
 
     private int index;
 
@@ -21,13 +29,27 @@ public class DeleteCommand extends Command {
         this.index = index;
     }
 
+    /**
+     * Executes the delete command by removing the expense at the given index.
+     *
+     * @param expenses the expense list to delete from
+     * @param ui the UI for displaying output
+     * @throws SpendTrackException if the index is out of range
+     */
     @Override
     public void execute(ExpenseList expenses, Ui ui) throws SpendTrackException {
+        assert expenses != null : "ExpenseList should not be null";
+        assert ui != null : "Ui should not be null";
+        assert index > 0 : "Index should be positive";
+
+        logger.info("Attempting to delete expense at index: " + index);
         if (index < 1 || index > expenses.size()) {
+            logger.warning("Delete failed: index " + index + " out of range");
             throw new SpendTrackException("Index " + index + " is out of range. "
                     + "There are " + expenses.size() + " expense(s).");
         }
         Expense removed = expenses.deleteExpense(index - 1);
+        logger.info("Expense deleted: " + removed);
         ui.showDeleteSuccess(removed);
     }
 }
